21dbc153d33f908792d6d39fe2c942f58b997140
Plamen Nedelchev <45600382+Plam3nn@users.noreply.github.com>
2025-03-06 11:16:09 +0200

    delete JS Fundamentals directory

--- JS Fundamentals/01.Arrays/Lab/P01.SumFirstAndLastArrayElements.js
+++
@@ -1,8 +0,0 @@
-function solve(input) {
-    let firstNumber = Number(input.shift());
-    let secondNumber = Number(input.pop());
-    
-    console.log(firstNumber + secondNumber);    
-}
-
-solve([`10`, `20`, `40`]);
